fdcfa7e80131d2316bee3ccfb21799418dfa8baa
Sameet-Ikram <90607111+Sameet-Ikram@users.noreply.github.com>
2023-05-13 18:03:54 +0500

    errors resolved

--- anigen/src/App.js
+++ anigen/src/App.js
@@ -1,41 +1,234 @@
-import React, {useState} from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar/Navbar'
-import Avatar from './components/CreateAvatar/CreateAvatar'
-import Navbar2 from './components/Navbar2/Navbar2'
-import Home from './components/Home/home'
-import Home2 from './components/Home2/Home2'
-import About from './components/About/about'
-import Difference from './components/Difference/difference'
-import Service from './components/Services/services'
-import Contact from './components/Contact/contact'
-import Login from './components/LogIn/Login'
-import TTS from './components/TTS/TTS'
-import Video from './components/Video/Video'
+import Navbar from './components/Navbar/Navbar';
+import Avatar from './components/CreateAvatar/CreateAvatar';
+import Navbar2 from './components/Navbar2/Navbar2';
+import Home from './components/Home/home';
+import Home2 from './components/Home2/Home2';
+import About from './components/About/about';
+import Difference from './components/Difference/difference';
+import Service from './components/Services/services';
+import Contact from './components/Contact/contact';
+import Login from './components/LogIn/Login';
+import TTS from './components/TTS/TTS';
+import Video from './components/Video/Video';
 import WebGL from './components/Web GL/WebGL';
-import Register from './components/Register/Register'
-import Footer from './components/Footer/footer'
-import {Route, Routes} from 'react-router-dom'
+import Register from './components/Register/Register';
+import Footer from './components/Footer/footer';
+import { Route, Routes, useRoutes } from 'react-router-dom';
+
 function App() {
+  const auth= localStorage.getItem("user");
+  const isAuthenticated = auth !== null;
+  const routes = isAuthenticated? (useRoutes([
+    {
+      path: '/',
+      element: (
+        <>
+          <Navbar />
+          <Home />
+        </>
+      ),
+    },
+    {
+      path: '/home',
+      element: (
+        <>
+          <Navbar />
+          <Home2 />
+        </>
+      ),
+    },
+    {
+      path: '/about',
+      element: (
+        <>
+          <Navbar />
+          <About />
+        </>
+      ),
+    },
+    {
+      path: '/difference',
+      element: (
+        <>
+          <Navbar />
+          <Difference />
+        </>
+      ),
+    },
+    {
+      path: '/avatar',
+      element: (
+        <>
+          <Navbar />
+          <Avatar />
+        </>
+      ),
+    },
+    {
+      path: '/TTS',
+      element: (
+        <>
+          <Navbar />
+          <TTS />
+        </>
+      ),
+    },
+    {
+      path: '/video',
+      element: (
+        <>
+          <Navbar />
+          <WebGL />
+        </>
+      ),
+    },
+    {
+      path: '/service',
+      element: (
+        <>
+          <Navbar />
+          <Service />
+        </>
+      ),
+    },
+    {
+      path: '/contact',
+      element: (
+        <>
+          <Navbar />
+          <Contact />
+        </>
+      ),
+    },
+    {
+      path: '/login',
+      element: (
+        <>
+          <Navbar />
+          <Login />
+        </>
+      ),
+    },
+    {
+      path: '/register',
+      element: (
+        <>
+          <Navbar />
+          <Register />
+        </>
+      ),
+    },
+  ])
+):(
+  useRoutes([
+    {
+      path: '/',
+      element: (
+        <>
+          <Navbar />
+          <Home />
+        </>
+      ),
+    },
+    {
+      path: '/home',
+      element: (
+        <>
+          <Navbar />
+          <Home />
+        </>
+      ),
+    },
+    {
+      path: '/about',
+      element: (
+        <>
+          <Navbar />
+          <About />
+        </>
+      ),
+    },
+    {
+      path: '/difference',
+      element: (
+        <>
+          <Navbar />
+          <Difference />
+        </>
+      ),
+    },
+    {
+      path: '/avatar',
+      element: (
+        <>
+          <Navbar />
+          <Home />
+        </>
+      ),
+    },
+    {
+      path: '/TTS',
+      element: (
+        <>
+          <Navbar />
+          <Home />
+        </>
+      ),
+    },
+    {
+      path: '/video',
+
+      element: (
+        <>
+          <Navbar />
+          <Home />
+        </>)
+    },
+    {
+      path: '/service',
+      element: (
+        <>
+          <Navbar />
+          <Service />
+        </>
+      ),
+    },
+    {
+      path: '/contact',
+      element: (
+        <>
+          <Navbar />
+          <Contact />
+        </>
+      ),
+    },
+    {
+      path: '/login',
+      element: (
+        <>
+          <Navbar />
+          <Login />
+        </>
+      ),
+    },
+    {
+      path: '/register',
+      element: (
+        <>
+          <Navbar />
+          <Register />
+        </>
+      ),
+    },
+  ])
+)
   return (
     <>
-  <Navbar/>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/home" element={<Home2/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/difference" element={<Difference/>}/>
-      <Route path="/avatar" element={<Avatar/>}/>
-      <Route path="/TTS" element={<TTS/>}/>
-       <Route path="/video" element={<WebGL/>}/> 
-      {/* <Route path="/video" element={<Video/>}/> */}
-      <Route path="/service" element={<Service/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/register" element={<Register/>}/>
-    </Routes>
-    <Footer/>
+      {routes}
+      <Footer />
     </>
   );
 }
